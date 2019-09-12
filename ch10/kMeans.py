78e33df3a1e6c6b84d1c0c6b232192f96f1014d9
zhangxiaogui <zxglive2006@126.com>
2019-09-13 00:28:01 +0800

    整理K-均值代码格式和python3匹配

--- ch10/kMeans.py
+++ ch10/kMeans.py
@@ -1,54 +1,64 @@
-'''
+# encoding: UTF-8
+"""
 Created on Feb 16, 2011
 k Means Clustering for Ch10 of Machine Learning in Action
 @author: Peter Harrington
-'''
+"""
 from numpy import *
+import matplotlib.pyplot as plt
 
-def loadDataSet(fileName):      #general function to parse tab -delimited floats
-    dataMat = []                #assume last column is target value
-    fr = open(fileName)
+
+def load_data_set(file_name):      # general function to parse tab -delimited floats
+    data_mat = []                    # assume last column is target value
+    fr = open(file_name)
     for line in fr.readlines():
-        curLine = line.strip().split('\t')
-        fltLine = map(float,curLine) #map all elements to float()
-        dataMat.append(fltLine)
-    return dataMat
+        cur_line = line.strip().split('\t')
+        flt_line = list(map(float, cur_line)) # map all elements to float()
+        data_mat.append(flt_line)
+    return data_mat
 
-def distEclud(vecA, vecB):
-    return sqrt(sum(power(vecA - vecB, 2))) #la.norm(vecA-vecB)
 
-def randCent(dataSet, k):
-    n = shape(dataSet)[1]
-    centroids = mat(zeros((k,n)))#create centroid mat
-    for j in range(n):#create random cluster centers, within bounds of each dimension
-        minJ = min(dataSet[:,j]) 
-        rangeJ = float(max(dataSet[:,j]) - minJ)
-        centroids[:,j] = mat(minJ + rangeJ * random.rand(k,1))
+def dist_euler(vec_a, vec_b):
+    return sqrt(sum(power(vec_a - vec_b, 2)))     # la.norm(vecA-vecB)
+
+
+def rand_cent(data_set, k):
+    n = shape(data_set)[1]
+    centroids = mat(zeros((k, n)))   # create centroid mat
+    for j in range(n):              # create random cluster centers, within bounds of each dimension
+        min_j = min(data_set[:, j])
+        max_j = max(data_set[:, j])
+        range_j = float(max_j - min_j)
+        centroids[:, j] = mat(min_j + range_j * random.rand(k, 1))
     return centroids
-    
-def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
-    m = shape(dataSet)[0]
-    clusterAssment = mat(zeros((m,2)))#create mat to assign data points 
-                                      #to a centroid, also holds SE of each point
-    centroids = createCent(dataSet, k)
-    clusterChanged = True
-    while clusterChanged:
-        clusterChanged = False
-        for i in range(m):#for each data point assign it to the closest centroid
-            minDist = inf; minIndex = -1
+
+
+def k_means(data_set, k, dist_meas=dist_euler, create_cent=rand_cent):
+    m = shape(data_set)[0]
+    cluster_assment = mat(zeros((m,2)))
+    # create mat to assign data points to a centroid, also holds SE of each point
+    centroids = create_cent(data_set, k)
+    cluster_changed = True
+    while cluster_changed:
+        cluster_changed = False
+        for i in range(m):  # for each data point assign it to the closest centroid
+            minDist = inf;
+            minIndex = -1
             for j in range(k):
-                distJI = distMeas(centroids[j,:],dataSet[i,:])
+                distJI = dist_meas(centroids[j, :], data_set[i, :])
                 if distJI < minDist:
                     minDist = distJI; minIndex = j
-            if clusterAssment[i,0] != minIndex: clusterChanged = True
-            clusterAssment[i,:] = minIndex,minDist**2
-        print centroids
-        for cent in range(k):#recalculate centroids
-            ptsInClust = dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]#get all the point in this cluster
-            centroids[cent,:] = mean(ptsInClust, axis=0) #assign centroid to mean 
-    return centroids, clusterAssment
+            if cluster_assment[i,0] != minIndex: cluster_changed = True
+            cluster_assment[i,:] = minIndex,minDist**2
+        print(centroids)
+        for cent in range(k):   # recalculate centroids
+            # get all the point in this cluster
+            ptsInClust = data_set[nonzero(cluster_assment[:, 0].A == cent)[0]]
+            centroids[cent,:] = mean(ptsInClust, axis=0)    # assign centroid to mean
+    return centroids, cluster_assment
 
-def biKmeans(dataSet, k, distMeas=distEclud):
+
+def biKmeans(dataSet, k, distMeas=dist_euler):
     m = shape(dataSet)[0]
     clusterAssment = mat(zeros((m,2)))
     centroid0 = mean(dataSet, axis=0).tolist()[0]
@@ -59,10 +69,10 @@ def biKmeans(dataSet, k, distMeas=distEclud):
         lowestSSE = inf
         for i in range(len(centList)):
             ptsInCurrCluster = dataSet[nonzero(clusterAssment[:,0].A==i)[0],:]#get the data points currently in cluster i
-            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            centroidMat, splitClustAss = k_means(ptsInCurrCluster, 2, distMeas)
             sseSplit = sum(splitClustAss[:,1])#compare the SSE to the currrent minimum
             sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:,0].A!=i)[0],1])
-            print "sseSplit, and notSplit: ",sseSplit,sseNotSplit
+            print("sseSplit, and notSplit: ",sseSplit,sseNotSplit)
             if (sseSplit + sseNotSplit) < lowestSSE:
                 bestCentToSplit = i
                 bestNewCents = centroidMat
@@ -70,8 +80,8 @@ def biKmeans(dataSet, k, distMeas=distEclud):
                 lowestSSE = sseSplit + sseNotSplit
         bestClustAss[nonzero(bestClustAss[:,0].A == 1)[0],0] = len(centList) #change 1 to 3,4, or whatever
         bestClustAss[nonzero(bestClustAss[:,0].A == 0)[0],0] = bestCentToSplit
-        print 'the bestCentToSplit is: ',bestCentToSplit
-        print 'the len of bestClustAss is: ', len(bestClustAss)
+        print('the bestCentToSplit is: ',bestCentToSplit)
+        print('the len of bestClustAss is: ', len(bestClustAss))
         centList[bestCentToSplit] = bestNewCents[0,:].tolist()[0]#replace a centroid with two best centroids 
         centList.append(bestNewCents[1,:].tolist()[0])
         clusterAssment[nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:]= bestClustAss#reassign new clusters, and SSE
@@ -87,7 +97,7 @@ def geoGrab(stAddress, city):
     params['location'] = '%s %s' % (stAddress, city)
     url_params = urllib.urlencode(params)
     yahooApi = apiStem + url_params      #print url_params
-    print yahooApi
+    print(yahooApi)
     c=urllib.urlopen(yahooApi)
     return json.loads(c.read())
 
@@ -101,20 +111,20 @@ def massPlaceFind(fileName):
         if retDict['ResultSet']['Error'] == 0:
             lat = float(retDict['ResultSet']['Results'][0]['latitude'])
             lng = float(retDict['ResultSet']['Results'][0]['longitude'])
-            print "%s\t%f\t%f" % (lineArr[0], lat, lng)
+            print("%s\t%f\t%f" % (lineArr[0], lat, lng))
             fw.write('%s\t%f\t%f\n' % (line, lat, lng))
-        else: print "error fetching"
+        else:
+            print("error fetching")
         sleep(1)
     fw.close()
-    
-def distSLC(vecA, vecB):#Spherical Law of Cosines
-    a = sin(vecA[0,1]*pi/180) * sin(vecB[0,1]*pi/180)
-    b = cos(vecA[0,1]*pi/180) * cos(vecB[0,1]*pi/180) * \
-                      cos(pi * (vecB[0,0]-vecA[0,0]) /180)
-    return arccos(a + b)*6371.0 #pi is imported with numpy
 
-import matplotlib
-import matplotlib.pyplot as plt
+
+def distSLC(vecA, vecB):    # Spherical Law of Cosines
+    a = sin(vecA[0,1]*pi/180) * sin(vecB[0,1]*pi/180)
+    b = cos(vecA[0,1]*pi/180) * cos(vecB[0,1]*pi/180) * cos(pi * (vecB[0,0]-vecA[0,0]) /180)
+    return arccos(a + b)*6371.0     # pi is imported with numpy
+
+
 def clusterClubs(numClust=5):
     datList = []
     for line in open('places.txt').readlines():
@@ -123,9 +133,8 @@ def clusterClubs(numClust=5):
     datMat = mat(datList)
     myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
     fig = plt.figure()
-    rect=[0.1,0.1,0.8,0.8]
-    scatterMarkers=['s', 'o', '^', '8', 'p', \
-                    'd', 'v', 'h', '>', '<']
+    rect = [0.1,0.1,0.8,0.8]
+    scatterMarkers = ['s', 'o', '^', '8', 'p', 'd', 'v', 'h', '>', '<']
     axprops = dict(xticks=[], yticks=[])
     ax0=fig.add_axes(rect, label='ax0', **axprops)
     imgP = plt.imread('Portland.png')
@@ -137,3 +146,14 @@ def clusterClubs(numClust=5):
         ax1.scatter(ptsInCurrCluster[:,0].flatten().A[0], ptsInCurrCluster[:,1].flatten().A[0], marker=markerStyle, s=90)
     ax1.scatter(myCentroids[:,0].flatten().A[0], myCentroids[:,1].flatten().A[0], marker='+', s=300)
     plt.show()
+
+
+if __name__ == '__main__':
+    print("ch10")
+    datMat = mat(load_data_set('testSet.txt'))
+    print(min(datMat[:, 0]))
+    print(min(datMat[:, 1]))
+    print(max(datMat[:, 0]))
+    print(max(datMat[:, 1]))
+    print(rand_cent(datMat, 2))
+    print(dist_euler(datMat[0], datMat[1]))
