b8081b525af41c0df6c8db16a0a53b58e9cb58d6
zhangxiaogui <zxglive2006@126.com>
2020-03-03 13:41:07 +0800

    整理第四章朴素贝叶斯代码

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -1,33 +1,38 @@
-'''
+# coding=utf-8
+"""
 Created on Oct 19, 2010
-
 @author: Peter
-'''
+"""
 from numpy import *
 
-def loadDataSet():
-    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+
+def load_data_set():
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
                  ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
                  ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
                  ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
                  ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-    classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
+    classVec = [0, 1, 0, 1, 0, 1]    # 1 is abusive, 0 not
     return postingList,classVec
-                 
+
+
 def createVocabList(dataSet):
     vocabSet = set([])  #create empty set
     for document in dataSet:
         vocabSet = vocabSet | set(document) #union of the two sets
     return list(vocabSet)
 
+
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
-        else: print "the word: %s is not in my Vocabulary!" % word
+        else:
+            print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
+
 
 def trainNB0(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
@@ -46,6 +51,7 @@ def trainNB0(trainMatrix,trainCategory):
     p0Vect = log(p0Num/p0Denom)          #change to log()
     return p0Vect,p1Vect,pAbusive
 
+
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     p1 = sum(vec2Classify * p1Vec) + log(pClass1)    #element-wise mult
     p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
@@ -53,7 +59,8 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
         return 1
     else: 
         return 0
-    
+
+
 def bagOfWords2VecMN(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
@@ -61,8 +68,9 @@ def bagOfWords2VecMN(vocabList, inputSet):
             returnVec[vocabList.index(word)] += 1
     return returnVec
 
+
 def testingNB():
-    listOPosts,listClasses = loadDataSet()
+    listOPosts,listClasses = load_data_set()
     myVocabList = createVocabList(listOPosts)
     trainMat=[]
     for postinDoc in listOPosts:
@@ -70,16 +78,18 @@ def testingNB():
     p0V,p1V,pAb = trainNB0(array(trainMat),array(listClasses))
     testEntry = ['love', 'my', 'dalmation']
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
-    print testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb)
+    print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
     testEntry = ['stupid', 'garbage']
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
-    print testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb)
+    print(testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+
 
 def textParse(bigString):    #input is big string, #output is word list
     import re
     listOfTokens = re.split(r'\W*', bigString)
     return [tok.lower() for tok in listOfTokens if len(tok) > 2] 
-    
+
+
 def spamTest():
     docList=[]; classList = []; fullText =[]
     for i in range(1,26):
@@ -107,21 +117,25 @@ def spamTest():
         wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
         if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
             errorCount += 1
-            print "classification error",docList[docIndex]
-    print 'the error rate is: ',float(errorCount)/len(testSet)
+            print("classification error",docList[docIndex])
+    print('the error rate is: ',float(errorCount)/len(testSet))
     #return vocabList,fullText
+
 
 def calcMostFreq(vocabList,fullText):
     import operator
     freqDict = {}
     for token in vocabList:
         freqDict[token]=fullText.count(token)
-    sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True) 
+    sortedFreq = sorted(freqDict.items(), key=operator.itemgetter(1), reverse=True)
     return sortedFreq[:30]       
 
-def localWords(feed1,feed0):
+
+def localWords(feed1, feed0):
     import feedparser
-    docList=[]; classList = []; fullText =[]
+    docList=[]
+    classList = []
+    fullText =[]
     minLen = min(len(feed1['entries']),len(feed0['entries']))
     for i in range(minLen):
         wordList = textParse(feed1['entries'][i]['summary'])
@@ -151,8 +165,9 @@ def localWords(feed1,feed0):
         wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
         if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
             errorCount += 1
-    print 'the error rate is: ',float(errorCount)/len(testSet)
+    print('the error rate is: ',float(errorCount)/len(testSet))
     return vocabList,p0V,p1V
+
 
 def getTopWords(ny,sf):
     import operator
@@ -162,10 +177,14 @@ def getTopWords(ny,sf):
         if p0V[i] > -6.0 : topSF.append((vocabList[i],p0V[i]))
         if p1V[i] > -6.0 : topNY.append((vocabList[i],p1V[i]))
     sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
-    print "SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**"
+    print("SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**")
     for item in sortedSF:
-        print item[0]
+        print(item[0])
     sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
-    print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**"
+    print("NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**")
     for item in sortedNY:
-        print item[0]
+        print(item[0])
+
+
+if __name__ == '__main__':
+    print("Run bayes finish")
