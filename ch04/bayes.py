e8a3685a24b6be013031c051000234fb5a19e56e
zhangxiaogui <zxglive2006@126.com>
2020-03-05 15:56:19 +0800

    将rss源改为本地nasa下载的文件

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -145,81 +145,92 @@ def spam_test():
     return vocab_list, full_text
 
 
-def calc_most_freq(vocab_list, full_text):
+def calc_most_freq(vocab_list, full_text, top_size=30):
     import operator
     freq_dict = {}
     for token in vocab_list:
         freq_dict[token] = full_text.count(token)
     sorted_freq = sorted(freq_dict.items(), key=operator.itemgetter(1), reverse=True)
-    return sorted_freq[:30]
+    return sorted_freq[:top_size]
 
 
-def local_words(feed1, feed0):
+def local_words(feed0, feed1):
     doc_list = []
     class_list = []
     full_text = []
-    min_len = min(len(feed1['entries']), len(feed0['entries']))
+    min_len = min(len(feed0['entries']), len(feed1['entries']))
     for i in range(min_len):
-        word_list = text_parse(feed1['entries'][i]['summary'])
-        doc_list.append(word_list)
-        full_text.extend(word_list)
-        class_list.append(1)
         word_list = text_parse(feed0['entries'][i]['summary'])
         doc_list.append(word_list)
         full_text.extend(word_list)
         class_list.append(0)
+        word_list = text_parse(feed1['entries'][i]['summary'])
+        doc_list.append(word_list)
+        full_text.extend(word_list)
+        class_list.append(1)
     vocab_list = create_vocab_list(doc_list)  # create vocabulary
-    top30_words = calc_most_freq(vocab_list, full_text)   # remove top 30 words
-    for pairW in top30_words:
+    top_words = calc_most_freq(vocab_list, full_text, 8)   # remove top words
+    print("top_words:{}".format(top_words))
+    for pairW in top_words:
         if pairW[0] in vocab_list:
             vocab_list.remove(pairW[0])
     total_size = 2 * min_len
+    test_set_size = int(0.2 * total_size)
+    print("total size:{}, test set size:{}".format(total_size, test_set_size))
     training_set = list(range(total_size))
     test_set = []           # create test set
-    test_set_size = int(0.2 * total_size)
     for i in range(test_set_size):
         rand_index = int(random.uniform(0, len(training_set)))
         test_set.append(training_set[rand_index])
-        del(training_set[rand_index])
+        del training_set[rand_index]
     train_mat = []
     train_classes = []
-    for docIndex in training_set:    # train the classifier (get probability) trainNB0
-        train_mat.append(bag_of_words_to_vec(vocab_list, doc_list[docIndex]))
-        train_classes.append(class_list[docIndex])
+    for doc_index in training_set:    # train the classifier (get probability) trainNB0
+        train_mat.append(bag_of_words_to_vec(vocab_list, doc_list[doc_index]))
+        train_classes.append(class_list[doc_index])
     p0_v, p1_v, p_spam = train_nb0(array(train_mat), array(train_classes))
     error_count = 0
-    for docIndex in test_set:        # classify the remaining items
-        word_vector = bag_of_words_to_vec(vocab_list, doc_list[docIndex])
-        if classify_nb(array(word_vector), p0_v, p1_v, p_spam) != class_list[docIndex]:
+    for doc_index in test_set:        # classify the remaining items
+        word_vector = bag_of_words_to_vec(vocab_list, doc_list[doc_index])
+        if classify_nb(array(word_vector), p0_v, p1_v, p_spam) != class_list[doc_index]:
             error_count += 1
+            print("classification error: ", doc_list[doc_index])
     print('the error rate is: ', round(float(error_count)/len(test_set), 2))
     return vocab_list, p0_v, p1_v
 
 
-def get_top_words(ny, sf):
-    vocab_list, p0_v, p1_v = local_words(ny, sf)
-    top_ny = []
-    top_sf = []
-    for i in range(len(p0_v)):
-        if p0_v[i] > -6.0:
-            top_sf.append((vocab_list[i], p0_v[i]))
-        if p1_v[i] > -6.0:
-            top_ny.append((vocab_list[i], p1_v[i]))
-    sorted_sf = sorted(top_sf, key=lambda pair: pair[1], reverse=True)
-    print("SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**")
-    for item in sorted_sf:
+def get_top_words(feed0, feed1):
+    vocab_list, p0_v, p1_v = local_words(feed0, feed1)
+    top_feed0 = []
+    top_feed1 = []
+    threshold = -3.5
+    for i in range(len(vocab_list)):
+        if p0_v[i] > threshold:
+            top_feed0.append((vocab_list[i], p0_v[i]))
+        if p1_v[i] > threshold:
+            top_feed1.append((vocab_list[i], p1_v[i]))
+    sorted_feed0 = sorted(top_feed0, key=lambda pair: pair[1], reverse=True)
+    print("feed0**feed0**feed0**feed0**feed0**feed0**feed0**feed0")
+    for item in sorted_feed0:
         print(item[0])
-    sorted_ny = sorted(top_ny, key=lambda pair: pair[1], reverse=True)
-    print("NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**")
-    for item in sorted_ny:
+    sorted_feed1 = sorted(top_feed1, key=lambda pair: pair[1], reverse=True)
+    print("feed1**feed1**feed1**feed1**feed1**feed1**feed1**feed1")
+    for item in sorted_feed1:
         print(item[0])
+
+
+def feed_test():
+    import feedparser
+    # breaking_news = feedparser.parse('https://www.nasa.gov/rss/dyn/breaking_news.rss')
+    # education_news = feedparser.parse("https://www.nasa.gov/rss/dyn/educationnews.rss")
+    # vocab_list, p_breaking_news, p_education_news = local_words(breaking_news, education_news)
+    local_breaking_news = feedparser.parse(r"./rss/breaking_news.rss")
+    local_education_news = feedparser.parse(r"./rss/educationnews.rss")
+    get_top_words(local_breaking_news, local_education_news)
 
 
 if __name__ == '__main__':
     # testing_nb()
-    spam_test()
-    # import feedparser
-    # breaking_news = feedparser.parse('https://www.nasa.gov/rss/dyn/breaking_news.rss')
-    # education_news = feedparser.parse("https://www.nasa.gov/rss/dyn/educationnews.rss")
-    # vocabList, p_nasa, p_cpp_blog = local_words(breaking_news, education_news)
+    # spam_test()
+    feed_test()
     print("Run bayes finish")
