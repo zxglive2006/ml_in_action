8aff57c1b587dc997c5f232d7933f48d1a7baa35
zhangxiaogui <zxglive2006@126.com>
2020-10-21 19:27:41 +0800

    整理均值方差计算reducer代码

--- ch15/mrMeanReducer.py
+++ ch15/mrMeanReducer.py
@@ -1,20 +1,23 @@
 """
 Created on Feb 21, 2011
 @author: Peter
+
+cd ch15
+python mrMeanMapper.py < inputFile.txt | python mrMeanReducer.py
 """
 import sys
-from numpy import mat, mean, power
 
 
 def read_input(file):
     for line in file:
         yield line.rstrip()
 
+
 # creates a list of input lines
-input = read_input(sys.stdin)
+my_input = read_input(sys.stdin)
 
 # split input lines into separate items and store in list of lists
-mapperOut = [line.split('\t') for line in input]
+mapperOut = [line.split('\t') for line in my_input]
 
 # accumulate total number of samples, overall sum and overall sum sq
 cumVal = 0.0
@@ -23,13 +26,13 @@ cumN = 0.0
 for instance in mapperOut:
     nj = float(instance[0])
     cumN += nj
-    cumVal += nj*float(instance[1])
-    cumSumSq += nj*float(instance[2])
-    
+    cumVal += nj * float(instance[1])
+    cumSumSq += nj * float(instance[2])
+
 # calculate means
-mean = cumVal/cumN
-meanSq = cumSumSq/cumN
+mean = cumVal / cumN
+meanSq = cumSumSq / cumN
 
 # output size, mean, mean(square values)
 print("%d\t%f\t%f" % (cumN, mean, meanSq))
-# print >> sys.stderr, "report: still alive"
+print("report: still alive", file=sys.stderr)
